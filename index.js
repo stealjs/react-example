4e1a580ec8392c865a958f047dbb31d068a7fc97
Chasen Le Hara <chasen@chasenlehara.com>
2019-03-02 13:30:25 -0800

    Update the Slack & forum links

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ class HelloMessage extends React.Component {
         <Title greeting={this.props.greeting} />
         <ul>
           <li>
-            To learn more about StealJS, visit <a href="http://stealjs.com/" target="_blank">
+            To learn more about StealJS, visit <a href="https://stealjs.com/" target="_blank">
               StealJS.com
             </a>
           </li>
           <li>
-            For live help with simple questions, check out <a href="https://gitter.im/stealjs/steal" target="_blank">
-              the Gitter chat
+            For live help with simple questions, check out <a href="https://www.bitovi.com/community/slack" target="_blank">
+              our Slack
             </a>
           </li>
           <li>
-            For more complex questions, post to <a href="http://forums.donejs.com/c/stealjs" target="_blank">
-              the forum
+            For more complex questions, post to <a href="https://forums.bitovi.com/c/stealjs" target="_blank">
+              our forums
             </a>
           </li>
         </ul>
